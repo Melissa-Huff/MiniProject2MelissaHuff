f2a3665e8d59d88e577e2527bb409982b1200009
Melissa Huff <m_huff@mail.fhsu.edu>
2024-02-26 13:45:22 -0800

    Created a chart to plot the churn rate for customers with and without tech support.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+# INF601 - Advanced Programming in Python
+# Melissa Huff
+# Mini Project 2
+
 import pandas as pd
+import matplotlib.pyplot as plt
 
-churn_analysis = pd.read_csv("Telco-Customer-Churn.csv", index_col=0, parse_dates=True)
+churn_analysis = pd.read_csv("Telco-Customer-Churn.csv")
 
-print(churn_analysis.head())
+tech_support_churn = churn_analysis.groupby('TechSupport')['Churn'].value_counts(normalize=True).unstack() * 100
+
+tech_support_churn.plot(kind='bar', stacked=True, figsize=(10, 6))
+plt.title('Churn Rate by Tech Support')
+plt.xlabel('Tech Support')
+plt.ylabel('Churn Rate (%)')
+plt.xticks(rotation=0)
+plt.legend(title='Churn', loc='upper right')
+plt.show()
+
